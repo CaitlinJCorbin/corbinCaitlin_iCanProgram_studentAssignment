de6f733245725c35fba014b994a8325a7a3ef76f
CaitlinJCorbin <58352759+CaitlinJCorbin@users.noreply.github.com>
2021-02-07 12:50:40 -0500

    Add files via upload

---
+++ main-1.cpp
@@ -0,0 +1,39 @@
+/* Program name: I Can Program
+* Author: Caitlin J. Corbin
+* Date last updated: Jan. 26, 2020
+* Purpose: Drawing a box shape thing
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "+---------------------------------------------------------------+\n"; //this module draws a shape
+    cout << " |                                                              |\n";
+    cout << " |                                                              |\n";
+    cout << " |  +-------------------------------------------------------+   |\n";
+    cout << " |  |              |                                        |   |\n";
+    cout << " |  |              |                                        |   |\n";
+    cout << " |  |              |                                        |   |\n";
+    cout << " |  |              |        Computer Science 1              |   |\n";
+    cout << " |  |              |        Module 3 Lab 1                  |   |\n";
+    cout << " |  |              |        Caitlin J. Corbin               |   |\n";
+    cout << " |  |              |        Due on Jan. 28, 2020            |   |\n";
+    cout << " |  |              |                                        |   |\n";
+    cout << " |  |              |                                        |   |\n";
+    cout << " |  |              |                                        |   |\n";
+    cout << " |  |              |                                        |   |\n";
+    cout << " |  +-------------------------------------------------------+   |\n";
+    cout << " |                                                              |\n";
+    cout << " +--------------------------------------------------------------+\n\n\n";
+    cout << " IIIII\n";
+    cout << "   I           ccc  aaaaa nnnn       pppp  rrrr  ooo   ggggg rrr  aaaaa mmm   mmm\n";
+    cout << "   I          c    a    a n   n      p   p r    o   o g    g r   a    a m  m m  m\n";
+    cout << "   I           ccc  aaaaa n   n      pppp  r     ooo   ggggg r    aaaaa m   m   m\n";
+    cout << "   I                                 p                     g\n";
+    cout << " IIIII                               p                  gggg\n";
+
+    return 0; //end
+}
